ee8ba33b052bb90426334ca439a68608b9f564e2
llsgit <31313085+llsgit@users.noreply.github.com>
2017-09-12 17:57:52 -0300

    dasdas

--- assets/js/pages/pedidos_pesquisa.js
+++ assets/js/pages/pedidos_pesquisa.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
+    $(document).on('click', '.btn-add', function(e)
+    {
+        e.preventDefault();
+
+        var controlForm = $('.controls form:first'),
+            currentEntry = $(this).parents('.entry:first'),
+            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+        newEntry.find('input').val('');
+        controlForm.find('.entry:not(:last) .btn-add')
+            .removeClass('btn-add').addClass('btn-remove')
+            .html('<span class="glyphicon glyphicon-minus"></span>');
+    }).on('click', '.btn-remove', function(e)
+    {
+		$(this).parents('.entry:first').remove();
+
+		e.preventDefault();
+		return false;
+	}).on('click', '.btn-submit', function(e)
+    {
+         var controlForm = $('.controls form:first');
+         alert(controlForm[0].value);
+    });;
+
+
     $('.date-picker').datepicker({
         orientation: "top auto",
         autoclose: true
