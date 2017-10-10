b936d01f050bc61ae902b4844fa2b155c4da2c2d
llsgit <31313085+llsgit@users.noreply.github.com>
2017-10-10 15:52:10 -0300

    ssaassasa

--- assets/js/pages/pedidos_resultado.js
+++ assets/js/pages/pedidos_resultado.js
@@ -45,7 +45,6 @@ $(document).ready(function() {
             { "data": "Fornecedor" },
             { "data": "Status" },
             { "data": "Dt_remessa" },
-            { "data": "Dt_retirada" },
             { "data": "Itens" }                        
         ],
         columnDefs: [
@@ -264,13 +263,13 @@ $(document).ready(function() {
 
 function format ( d ) {
     var table_return = '<table cellpadding="5" cellspacing="0" border="0" style="width: 100%;padding-left:100px;">'+
-                 '<thead><tr><th style="width:15px">#</th><th style="width:20px">Item</th><th style="width:130px">Material-lote</th><th style="width:130px">Material MAV</th><th style="width:35px">Qtd. Pedido</th><th style="width:35px">Unidade</th><th style="width:35px">Status</th><th style="width:40px">Nr. Agendamento</th><th style="width:40px">Solicitante</th></tr></thead>';
+                 '<thead><tr><th style="width:15px">#</th><th style="width:20px">Item</th><th style="width:130px">Material-lote</th><th style="width:130px">Material MAV</th><th style="width:35px">Qtd. Pedido</th><th style="width:35px">Unidade</th><th style="width:35px">Status</th><th style="width:35px">Coleta</th><th style="width:40px">Nr. Agendamento</th><th style="width:40px">Solicitante</th></tr></thead>';
     if (d.Itens.length == null) {
-        table_return = table_return + '<tr><td><input type="checkbox" id="'+d.Pedido+'_'+d.Itens.Item+'" '+d.Itens.Item_Checked+' value="'+d.Pedido+'_'+d.Itens.Item+'"></td><td>'+d.Itens.Item+'</td><td>'+d.Itens.Material_Lote+'</td><td>'+d.Itens.Material_Mav+'</td><td>'+d.Itens.Quantidade+'</td><td>'+d.Itens.Unidade+'</td><td>'+d.Itens.Status_Item+'</td><td><a href="javascript:editPedido('+d.Itens.Agendamento.split("/")[0]+');" title="Editar/Cancelar">'+d.Itens.Agendamento+'</a></td><td>'+d.Itens.Solicitante+'</td></tr>';
+        table_return = table_return + '<tr><td><input type="checkbox" id="'+d.Pedido+'_'+d.Itens.Item+'" '+d.Itens.Item_Checked+' value="'+d.Pedido+'_'+d.Itens.Item+'"></td><td>'+d.Itens.Item+'</td><td>'+d.Itens.Material_Lote+'</td><td>'+d.Itens.Material_Mav+'</td><td>'+d.Itens.Quantidade+'</td><td>'+d.Itens.Unidade+'</td><td>'+d.Itens.Status_Item+'</td><td>'+d.Dt_retirada+'</td><td>'+d.Itens.Status_Item+'</td><td><a href="javascript:editPedido('+d.Itens.Agendamento.split("/")[0]+');" title="Editar/Cancelar">'+d.Itens.Agendamento+'</a></td><td>'+d.Itens.Solicitante+'</td></tr>';
     }
     else{
         for (var i = 0; i < d.Itens.length; i++) {
-            table_return = table_return + '<tr><td><input type="checkbox" id="'+d.Pedido+'_'+d.Itens[i].Item+'" '+d.Itens[i].Item_Checked+' value="'+d.Pedido+'_'+d.Itens[i].Item+'"></td><td>'+d.Itens[i].Item+'</td><td>'+d.Itens[i].Material_Lote+'</td><td>'+d.Itens[i].Material_Mav+'</td><td>'+d.Itens[i].Quantidade+'</td><td>'+d.Itens[i].Unidade+'</td><td>'+d.Itens[i].Status_Item+'</td><td><a href="javascript:editPedido('+d.Itens[i].Agendamento.split("/")[0]+');" title="Editar/Cancelar">'+d.Itens[i].Agendamento+'</a></td><td>'+d.Itens[i].Solicitante+'</td></tr>';
+            table_return = table_return + '<tr><td><input type="checkbox" id="'+d.Pedido+'_'+d.Itens[i].Item+'" '+d.Itens[i].Item_Checked+' value="'+d.Pedido+'_'+d.Itens[i].Item+'"></td><td>'+d.Itens[i].Item+'</td><td>'+d.Itens[i].Material_Lote+'</td><td>'+d.Itens[i].Material_Mav+'</td><td>'+d.Itens[i].Quantidade+'</td><td>'+d.Itens[i].Unidade+'</td><td>'+d.Itens[i].Status_Item+'</td><td>'+d.Dt_retirada+'</td><td><a href="javascript:editPedido('+d.Itens[i].Agendamento.split("/")[0]+');" title="Editar/Cancelar">'+d.Itens[i].Agendamento+'</a></td><td>'+d.Itens[i].Solicitante+'</td></tr>';
         }
 
     }
