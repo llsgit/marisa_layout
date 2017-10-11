c2c728f7215f0ae09472da5252c503c8c2a1e19e
llsgit <31313085+llsgit@users.noreply.github.com>
2017-10-11 17:14:49 -0300

    okkk

--- assets/js/pages/usuarios.js
+++ assets/js/pages/usuarios.js
@@ -1,24 +1,35 @@
 $(document).ready(function() {
 	$('.edit').hide();
-	// Datatables
-   /* 	"paging": false,
-    var table = $('#table_data').DataTable({
-	    buttons: {
-	        buttons: [
-	            'new',
-	            {
-	            	text: 'Novo',
-	            	className: 'btn btn-primary',
-	            	action: function ( dt ) {
-	                    console.log( 'My custom button!' );
-	                }
-	            }
-	        ]
-	    }
-    }); */
-    $(document).on('click', '.btn-primary', function(e){
-		alert('ok');
+    $(document).on('click', '.btn-add', function(e)
+    {
+        e.preventDefault();
+        var controlForm  = $('.controls:first'),
+            currentEntry = $('.entry:first'),
+            newForm = $(controlForm.clone()).appendTo(currentEntry);
+        currentEntry.find('.btn-remove')
+            .removeClass('btn-remove').addClass('btn-add')
+            .html('<span class="glyphicon glyphicon-plus"></span>');          
+        newForm.find('input').val('');
+        currentEntry.find('.controls:not(:last) .btn-add')
+            .removeClass('btn-add').addClass('btn-remove')
+            .html('<span class="glyphicon glyphicon-minus"></span>');
+    }).on('click', '.btn-remove', function(e)
+    {
+		$(this).parents('.controls:first').remove();
+
+		e.preventDefault();
+		return false;
+	}).on('click', '.btn-primary', function(e){
 		$('.list').hide();
 		$('.edit').show();
     });
+
+	$( ".perfil" ).change(function() {
+		if ('FORNECEDOR' != this.value) {
+			$('.fornecedores').hide();
+		}
+		else{
+			$('.fornecedores').show();	
+		}
+	});
 });
