63991861ed32667f2a42cf517d9975d0d45327b1
llsgit <31313085+llsgit@users.noreply.github.com>
2017-10-27 11:33:04 -0200

    OK
    OK

--- assets/js/pages/usuarios.js
+++ assets/js/pages/usuarios.js
@@ -41,9 +41,16 @@ $(document).ready(function() {
 
     $('#table_data tbody').on('click', 'img.open_details', function () {
         var tr = $(this).closest('tr');
-        alert(tr);
         var row = table.row( tr );
-        alert(row.id);
+        if ( row.child.isShown() ) {
+            row.child.hide();
+            tr.removeClass('details');
+        }
+        else {
+            // Open this row
+            row.child( format(row.data()) ).show();
+            tr.addClass('details');
+        }                
     });
 
 	$('.edit').hide();
@@ -80,3 +87,19 @@ $(document).ready(function() {
 		}
 	});
 });
+
+function format ( d ) {
+    var table_return = '<table cellpadding="0" cellspacing="0" border="0" style="width: 100%;padding-left:0px;">'+
+                 '<thead><tr><th style="width:12px">#</th><th style="width:18px">Item</th><th style="width:130px">Material-lote</th><th style="width:130px">Material MAV</th><th style="width:30px">Qtd. Pedido</th><th style="width:30px">Unidade</th><th style="width:30px">Status</th><th style="width:30px">Coleta</th><th style="width:35px">Nr. Agendamento</th><th style="width:35px">Solicitante</th></tr></thead>';
+    if (d.Itens.length == null) {
+        table_return = table_return + '<tr><td><input type="checkbox" id="'+d.Pedido+'_'+d.Itens.Item+'" '+d.Itens.Item_Checked+' value="'+d.Pedido+'_'+d.Itens.Item+'"></td><td>'+d.Itens.Item+'</td><td>'+d.Itens.Material_Lote+'</td><td>'+d.Itens.Material_Mav+'</td><td>'+d.Itens.Quantidade+'</td><td>'+d.Itens.Unidade+'</td><td>'+d.Itens.Status_Item+'</td><td>'+d.Dt_retirada+'</td><td><a href="javascript:editPedido('+d.Itens.Agendamento.split("/")[0]+');" title="Editar/Cancelar">'+d.Itens.Agendamento+'</a></td><td>'+d.Itens.Solicitante+'</td></tr>';
+    }
+    else{
+        for (var i = 0; i < d.Itens.length; i++) {
+            table_return = table_return + '<tr><td><input type="checkbox" id="'+d.Pedido+'_'+d.Itens[i].Item+'" '+d.Itens[i].Item_Checked+' value="'+d.Pedido+'_'+d.Itens[i].Item+'"></td><td>'+d.Itens[i].Item+'</td><td>'+d.Itens[i].Material_Lote+'</td><td>'+d.Itens[i].Material_Mav+'</td><td>'+d.Itens[i].Quantidade+'</td><td>'+d.Itens[i].Unidade+'</td><td>'+d.Itens[i].Status_Item+'</td><td>'+d.Dt_retirada+'</td><td><a href="javascript:editPedido('+d.Itens[i].Agendamento.split("/")[0]+');" title="Editar/Cancelar">'+d.Itens[i].Agendamento+'</a></td><td>'+d.Itens[i].Solicitante+'</td></tr>';
+        }
+
+    }
+    table_return = table_return + '</table>';
+    return table_return;
+}
